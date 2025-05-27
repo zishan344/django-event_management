d56dbd51c2b14c5aceb9347fea34a85e155c0d57
zishan344 <zishanahmed344@gmail.com>
2025-05-27 14:24:28 +0600

    Set DEBUG to True  for production readiness

--- event_management/settings.py
+++ event_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%%$%87i_2cknxdhk*b3t1164c@6i0r)v(e*85^4!ij7d7s)8wu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL='users.CustomUser'
