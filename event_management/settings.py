94e0ec1649411755d90ef95796d5a5126e7510ca
zishan344 <zishanahmed344@gmail.com>
2025-10-17 11:41:58 +0600

    rmv pytcopg2-2.9.10

--- event_management/settings.py
+++ event_management/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-     "whitenoise.runserver_nostatic",
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'events',
     'users',
