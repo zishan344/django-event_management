0e5659301407eb6d80de7978a2861226f6369774
zishan344 <zishanahmed344@gmail.com>
2025-10-17 10:50:13 +0600

    add vercel

--- event_management/settings.py
+++ event_management/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL='users.CustomUser'
-CSRF_TRUSTED_ORIGINS=["https://*.onrender.com","http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS=["https://*.onrender.com","http://127.0.0.1:8000", "https://*.vercel.app/"]
 
 # Application definition
 INSTALLED_APPS = [
